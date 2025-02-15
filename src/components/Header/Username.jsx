9d8a70991e6b348dc71a2dc501934481812d04fe
Veronika Krylova <Lapshina.nika@yandex.ru>
2025-02-15 19:11:03 +0300

    fix: avatar round

--- src/components/Header/Username.jsx
+++ src/components/Header/Username.jsx
@@ -4,8 +4,10 @@ import { userInfo } from "../../store/userInfo";
 export const Username = observer(() => {
     return (
         <div className="flex flex-row items-center gap-2">
-            <div className="w-10 h-10 bg-primary rounded-full">
-                <img src={userInfo.getAvatarUrl()}></img>
+            <div className="avatar">
+                <div className="w-10 h-10 bg-primary rounded-full">
+                    <img src={userInfo.getAvatarUrl()}></img>
+                </div>
             </div>
             <div className="text-bold">{userInfo.name}</div>
         </div>
