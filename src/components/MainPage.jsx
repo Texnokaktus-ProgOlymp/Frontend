9b08d66506d00fae0a0196c920300bb1be9cce15
Veronika Krylova <Lapshina.nika@yandex.ru>
2025-02-17 22:04:29 +0300

    feat: register

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -15,16 +15,25 @@ export const MainPage = observer(() => {
         return (
             <>
                 <Header title={"Информация об участнике"} />
-                <div className="min-h-screen w-full flex justify-center items-center">
-                    <div className="card bg-primary-content w-1/2 min-h-1/3 shadow-sm">
-                        <div className="card-body flex gap-2">
+                <div className="w-full flex justify-center items-center h-96">
+                    <div className="card bg-primary-content max-w-3xl m-4 min-h-1/3 shadow-sm">
+                        <div className="card-body flex gap-4">
                             <h2 className="card-title self-center">
                                 Вы успешно зарегистрированы
                             </h2>
                             <p className="card-text self-center">
-                                Отборочный тур продлится с 20.02.2025 по 14.03.2025
+                                Отборочный тур продлится с 20.02.2025 по
+                                14.03.2025
                             </p>
                             <div className="justify-center card-actions">
+                                <button
+                                    className="btn btn-outline"
+                                    onClick={() => {
+                                        userInfo.logout();
+                                    }}
+                                >
+                                    Сменить аккаунт
+                                </button>
                                 <a
                                     className="btn btn-primary"
                                     target="_blank"
