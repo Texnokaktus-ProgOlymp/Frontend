99a48ba9e276b9f1e0e8c9d8c4ab4da729e99e62
Artem Krylov <kav128@outlook.com>
2026-02-16 22:52:36 +0300

    update OAuth

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -16,10 +16,10 @@ export const LandingPage = observer(() => {
             {
                 client_id: "c4d941e7f402461dac9944a569328c24",
                 response_type: "code",
-                redirect_uri: "https://yid.progolymp.cttit.ru",
+                redirect_uri: "https://progolymp.cttit.ru/oauth/yandex/helper",
                 force_confirm: true,
             },
-            "https://yid.progolymp.cttit.ru",
+            "https://progolymp.cttit.ru",
             {
                 view: "button",
                 parentId: "container",
@@ -40,14 +40,14 @@ export const LandingPage = observer(() => {
             <div className="hero-content text-center">
                 <div className="w-full max-w-lg">
                     <h1 className="text-3xl font-bold">
-                        Олимпиада по информатике и программированию 2025
+                        Олимпиада по информатике и программированию 2026
                     </h1>
                     <p className="py-6">
                         Для прохождения регистрации авторизуйтесь через Яндекс ID
                     </p>
                     <p className="py-6">
                         Выбранная на данном этапе учетная запись Яндекс ID будет использована для решения задач
-                        отборочного и очного тура
+                        отборочного тура
                     </p>
                     <div id="container" className="max-w-1/3"></div>
                 </div>
